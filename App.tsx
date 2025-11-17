e62cec1ea9d60a68512fb9e903489918c9c67e75
PrissyCorrea <pris.pcd@gmail.com>
2025-11-17 14:09:50 -0300

    screen adjust

--- App.tsx
+++ App.tsx
@@ -5,3 +5,26 @@ export default function App() {
   return <SplashScreen />;
 }
 
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { SplashScreen } from './src/screens/splash';
+
+// const Stack = createStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         initialRouteName="Splash"  // ← SplashScreen como tela inicial
+//         screenOptions={{
+//           headerShown: false,
+//         }}
+//       >
+//         <Stack.Screen name="Splash" component={SplashScreen} />
+//         {/* Adicione suas outras telas aqui */}
+//         {/* <Stack.Screen name="Main" component={MainScreen} /> */}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
