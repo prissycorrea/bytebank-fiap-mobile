19b5e46ae147e264156b16e7f337ec3fdaeb93c6
Júlio Magalhães <julio.magalhaes@maps.com.br>
2025-11-24 17:32:36 -0300

    fix: ajustando configs

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,8 @@
+// Learn more https://docs.expo.dev/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
-
-// Adiciona suporte para extensões de arquivo .cjs
 defaultConfig.resolver.sourceExts.push("cjs");
-
-// Desativa temporariamente uma funcionalidade para contornar um bug no Metro
-// que afeta a resolução de pacotes do Firebase.
 defaultConfig.resolver.unstable_enablePackageExports = false;
 
 module.exports = defaultConfig;
